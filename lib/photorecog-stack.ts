e3434434597e5c78405589b9244303276f419c41
shamaz332 <shamazsaeed332@gmail.com>
2021-02-17 22:31:38 +0500

    Lambda for service

--- lib/photorecog-stack.ts
+++ lib/photorecog-stack.ts
@@ -81,5 +81,24 @@ export class PhotorecogStack extends cdk.Stack {
         resources: ["*"],
       })
     );
+    // =====================================================================================
+    // Lambda for Synchronous Front End
+    // =====================================================================================
+    ​
+    const serviceFn = new lambda.Function(this, 'serviceFunction', {
+      code: lambda.Code.fromAsset('serviceLambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      environment: {
+        "TABLE": imageSortTable.tableName,
+        "BUCKET": imageBucket.bucketName,
+        "RESIZEDBUCKET": resizedBucket.bucketName
+      },
+    });
+    ​
+    imageBucket.grantWrite(serviceFn);
+    resizedBucket.grantWrite(serviceFn);
+    imageSortTable.grantReadWriteData(serviceFn);
+
   }
 }
